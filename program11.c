cbc44f4485abb9018797e99336bcc596db152ebb
snehalp1506 <117083768+snehalp1506@users.noreply.github.com>
2022-10-31 23:27:09 +0530

    corrected syntax

--- program11.c
+++ program11.c
@@ -7,14 +7,14 @@ op not a leap year
 
 #include<stdio.h>
 int main(){
-	int x;
+	int x=0;
 	printf("Enter Year: \n");
 	scanf("%d", &x);
 
 	if(x%400==0 && x%100!=0){
 		printf("It is a Leap Year \n");
 	}
-	if(x%4==0 && x%100==0){
+	else if(x%4==0 && x%100==0){
 		printf("It is a Leap Year \n");
 	}
 	
